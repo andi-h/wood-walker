93db5d48b2e323bd080877153c64ed0c8292de09
Andreas Hirtenlehner <andreas.hirtenlehner@heap-engineering.at>
2026-06-10 18:20:51 +0200

    update container to new path; change to locl WiFi hotspot; copy src files in release container instead of mounting

--- 02_dev_environment/start-dev-env.sh
+++ 02_dev_environment/start-dev-env.sh
@@ -9,22 +9,22 @@ if toolbox list | grep -q "$TOOLBOX_NAME"; then
 else
     # Create a new toolbox
     echo "Creating a new toolbox: $TOOLBOX_NAME"
-    
+
     SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     podman build -t ros-toolbox --build-arg ROS_DISTRO=${ROS_DISTRO} $SCRIPT_DIR
     toolbox create -i ros-toolbox "$TOOLBOX_NAME"
 
     toolbox run --container "$TOOLBOX_NAME" env ROS_DISTRO=${ROS_DISTRO} bash <<EOF
     code --install-extension ms-vscode-remote.remote-containers
-    
+
     systemctl --user enable podman.socket
     systemctl --user start podman.socket
-    
+
     source /opt/ros/${ROS_DISTRO}/setup.bash
 
     # Build package timber_crane_description to use meshes in rviz2
     echo "Building timber_crane_description..."
-    cd $SCRIPT_DIR/../02_ros-demo
+    cd $SCRIPT_DIR/../03_vehicle_runtime
     colcon build --packages-select timber_crane_description
     source install/setup.bash
 
@@ -33,12 +33,12 @@ else
     cd $SCRIPT_DIR
     colcon build
     source install/setup.bash
-    
+
     echo "Init rosdep..."
     rosdep init \
      && rosdep update --rosdistro ${ROS_DISTRO}  \
      && rosdep install --from-paths ./ -i -y --rosdistro ${ROS_DISTRO}
-    
+
     echo "VS Code and Podman installation inside the toolbox is complete!"
 EOF
 
